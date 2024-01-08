44267aef633c15eaa7bc65f41f9c62fe80a95f74
DanielLeal619 <lealdaniel05@gmail.com>
2024-01-08 16:05:43 -0600

    Logo error

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,7 +6,7 @@ const Banner = () => {
             <img src="https://i.postimg.cc/4NMT1VPr/navbar.png" alt="" />
             <Link to="/pokedex">
                 <div className="banner__img">
-                    <img className="banner__letter" src="https://camo.githubusercontent.com/7f1f1e69bef239378a28e8aca7d1d7bd0890d37a7871d01135e2d044da6e2157/68747470733a2f2f692e696d6775722e636f6d2f415975745a4f462e706e67" alt="" />
+                    <img className="banner__letter" src="https://d112y698adiu2z.cloudfront.net/photos/production/software_photos/002/436/896/datas/original.png" alt="" />
                 </div>
             </Link>
         </div>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -17,7 +17,7 @@ const HomePage = () => {
     navigate("/pokedex")
   }
 
-  const urlImg = "https://camo.githubusercontent.com/7f1f1e69bef239378a28e8aca7d1d7bd0890d37a7871d01135e2d044da6e2157/68747470733a2f2f692e696d6775722e636f6d2f415975745a4f462e706e67"
+  const urlImg = "https://d112y698adiu2z.cloudfront.net/photos/production/software_photos/002/436/896/datas/original.png"
 
   return (
     <article className="home">
